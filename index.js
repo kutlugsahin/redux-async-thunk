025084c38cdd135bf473a702fe695a1463329060
kutlu <kutlugsahin@gmail.com>
2017-02-27 02:11:13 +0300

    async fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createAsyncThunkMiddleware(extraArgument) {
   return ({ dispatch, getState }) => next => action => {
     if (typeof action === 'function') {
       if (Object.getPrototypeOf(action).constructor.name === 'AsyncFunction') {
-        return await action(dispatch, getState, extraArgument);
+        return (async function() { await action(dispatch, getState, extraArgument); })();
       }
 
       if (Object.getPrototypeOf(action).constructor.name === 'GeneratorFunction') {
